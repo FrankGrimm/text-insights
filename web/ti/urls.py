d8dfc351b683d11c403b13b7b5032b8a24d1c371
Frank Grimm <frankgrimm@gmail.com>
2013-10-24 19:42:57 +0200

    Template test

--- web/ti/urls.py
+++ web/ti/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('ti.views', 
     url(r'^$', 'home', name='hello'),
+    url(r'^template$', 'template', name='template'),
 )
--- web/ti/views.py
+++ web/ti/views.py
@@ -1,6 +1,13 @@
 # views
 
 from django.http import HttpResponse
+from django.template import Context, Template
 
 def home(request):
     return HttpResponse("Hello")
+
+def template(request):
+    t = Template('Hello {{ name }}')
+    c = Context({'name': 'yourname'})
+    html = t.render(c)
+    return HttpResponse(html)
