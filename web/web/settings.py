f2d1f6845a618eb1c2fe07530e77407c4d96f6d8
Frank Grimm <frankgrimm@gmail.com>
2013-10-30 20:50:55 +0100

    Do not use timezones for db timestamps

--- web/web/settings.py
+++ web/web/settings.py
@@ -1,5 +1,7 @@
 # Django settings for web project.
 import os
+import warnings
+import exceptions 
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -52,7 +54,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False 
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
@@ -174,3 +176,4 @@ LOGGING = {
         },
     }
 }
+
